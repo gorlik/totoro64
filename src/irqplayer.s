8c27bd81ebee0cb4c0313889af3d1613fed1f75b
gorlik <44734244+gorlik@users.noreply.github.com>
2021-09-04 20:31:11 -0700

    add change instrument and semitone offset to player

--- src/irqplayer.s
+++ src/irqplayer.s
@@ -24,6 +24,8 @@ _vpb:
 	.res 1
 ctmp:
 	.res 1
+_l_offset:
+	.res 1
 
 
 .segment	"CODE"
@@ -58,15 +60,20 @@ FTablePalHi:
 .if IPDebug =1
 	lda $d020
 	sta ctmp
-	lda #1
+	lda _instr1
+	lsr a
+	lsr a			
+	lsr a
+	lsr a
 	sta $d020
 .endif
 	lda _time1
-	bne inc_end		; timer not expired
+	bne time_inc_end		; timer not expired
 next:
 	ldy #0
 	lda (_t1ptr),y
 	sta tmp
+	jsr inc_t1ptr 		; _t1ptr point to the next byte
 	and #$f0
 	bne parse_cmd
 	;; new note
@@ -85,16 +92,15 @@ add_more:
 	bne add_more
 	sta _time1
 
-	iny
-	clc
 	lda (_t1ptr),y
 	bne not_pause
 	lda _instr1
 	sta SID_Ctl1
-	jmp inc_addr
+	jmp inc_ptr
 
 not_pause:
 	adc _loop1
+	adc _l_offset
 	tax
 	lda FTablePalHi,x
 	sta SID_S1Hi
@@ -103,15 +109,9 @@ not_pause:
 	lda _instr1
 	ora #1
 	sta SID_Ctl1
-inc_addr:
-	clc
-	lda _t1ptr
-	adc #2
-	sta _t1ptr
-	bcc inc_end
-	inc _t1ptr+1
-
-inc_end:
+inc_ptr:
+	jsr inc_t1ptr
+time_inc_end:
 	dec _time1
 	lda _time1
 	cmp #2
@@ -138,6 +138,10 @@ parse_cmd:
 	lda tmp
 	cmp #$ff
 	beq cmd_ff
+	cmp #$fe
+	beq cmd_fe
+	cmp #$fd
+	beq cmd_fd
 	jmp next
 	
 cmd_ff:	 			; end of stream
@@ -148,5 +152,26 @@ cmd_ff:	 			; end of stream
 	lda _next_loop1
 	sta _loop1		; set the new offset
 	jmp next
+
+cmd_fe:	 			; change instrument
+	lda (_t1ptr),y
+	sta _instr1
+	jsr inc_t1ptr
+	jmp next
+cmd_fd:	 			; change offset
+	lda (_t1ptr),y
+	sta _l_offset
+	jsr inc_t1ptr
+	jmp next
+	
+
+inc_t1ptr:
+	inc _t1ptr
+	bne t1inc_end
+	inc _t1ptr+1
+t1inc_end:
+	rts
 .endproc
+
+	
 
