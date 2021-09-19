365ca59d8cab19c01a652521a156bf170ec0e60c
gorlik <44734244+gorlik@users.noreply.github.com>
2021-09-18 22:40:19 -0700

    add/fix copyright banners

--- src/irqplayer.s
+++ src/irqplayer.s
@@ -1,3 +1,23 @@
+;******************************************************************************
+;*  TOTORO64                                                                  *
+;*  A Studio Ghibli inspired game for the Commodore 64                        *
+;*  Copyright 2021 Gabriele Gorla                                             *
+;*                                                                            *
+;*  This program is free software: you can redistribute it and/or modify      *
+;*  it under the terms of the GNU General Public License as published by      *
+;*  the Free Software Foundation, either version 3 of the License, or         *
+;*  (at your option) any later version.                                       *
+;*                                                                            *
+;*  TOTORO64 is distributed in the hope that it will be useful,               *
+;*  but WITHOUT ANY WARRANTY; without even the implied warranty of            *
+;*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             *
+;*  GNU General Public License for more details.                              *
+;*                                                                            *
+;*  You should have received a copy of the GNU General Public License         *
+;*  along with TOTORO64.  If not, see <http://www.gnu.org/licenses/>.         *
+;*                                                                            *
+;******************************************************************************
+
 .include "c64.inc"
 	
 .define IPDebug     0
--- src/variables.s
+++ src/variables.s
@@ -1,20 +1,20 @@
 ;******************************************************************************
-;*  GTERM - GGLABS TERMINAL                                                   *
-;*  A fast 80 columns terminal for the C64                                    *
-;*  Copyright 2019 Gabriele Gorla                                             *
+;*  TOTORO64                                                                  *
+;*  A Studio Ghibli inspired game for the Commodore 64                        *
+;*  Copyright 2021 Gabriele Gorla                                             *
 ;*                                                                            *
 ;*  This program is free software: you can redistribute it and/or modify      *
 ;*  it under the terms of the GNU General Public License as published by      *
 ;*  the Free Software Foundation, either version 3 of the License, or         *
 ;*  (at your option) any later version.                                       *
 ;*                                                                            *
-;*  GTERM is distributed in the hope that it will be useful,                  *
+;*  TOTORO64 is distributed in the hope that it will be useful,               *
 ;*  but WITHOUT ANY WARRANTY; without even the implied warranty of            *
 ;*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             *
 ;*  GNU General Public License for more details.                              *
 ;*                                                                            *
 ;*  You should have received a copy of the GNU General Public License         *
-;*  along with GTERM.  If not, see <http://www.gnu.org/licenses/>.            *
+;*  along with TOTORO64.  If not, see <http://www.gnu.org/licenses/>.         *
 ;*                                                                            *
 ;******************************************************************************
 
